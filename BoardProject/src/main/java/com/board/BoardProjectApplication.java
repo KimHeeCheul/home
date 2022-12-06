a1463b1fcbaf31eb876b07c6a1fdd611dca304ca
KimHeeCheul <kim11403@naver.com>
2022-12-06 13:53:35 +0900

    ..

---
+++ BoardProject/src/main/java/com/board/BoardProjectApplication.java
@@ -0,0 +1,24 @@
+package com.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class BoardProjectApplication {
+
+	public static void main(String[] args) throws Exception {
+		ConfigurableApplicationContext context= SpringApplication.run(BoardProjectApplication.class, args);
+		BoardService boardService=(BoardService)context.getBean("boardService");
+		System.out.println(boardService);
+		System.out.println(boardService.selectAll());
+		System.out.println(boardService.insert(new Board(13, "", "", "", "")));
+		System.out.println(boardService.delete(9));
+		System.out.println(boardService.update(new Board(12, "test", "test", "test", "2022/11/29")));
+		System.out.println(boardService.selectByNo(12));
+		/*
+		 * HelloServlet 작성 실행 
+		 */
+	}
+
+}
---
+++ BoardProject/src/main/java/com/board/BoardService.java
@@ -0,0 +1,35 @@
+package com.board;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("boardService")
+public class BoardService {
+	@Autowired
+	private BoardDao boardDao;
+	public BoardService() {
+		
+	}
+	public Board selectByNo(int n) throws Exception{
+	  return boardDao.selectByNo(n);
+	}
+	
+	public List<Board> selectAll()throws Exception{
+		return boardDao.selectAll();
+	}
+
+	public int insert(Board board) throws Exception{
+		return boardDao.insert(board);
+	}
+	
+	public int update(Board board) throws Exception{
+		return boardDao.update(board);
+	}
+	
+	public int delete(int no) throws Exception{
+		return boardDao.delete(no);
+	}
+	
+}
